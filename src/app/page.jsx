f3357e695c9379305397ebb13c7374b55bf8fd4b
StarkPrince <games.princeraj@gmail.com>
2023-07-06 15:24:25 +0530

    request

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,8 +13,8 @@ export default function Home() {
                 <Section type="hero" />
                 <Section type="how-it-works" title="How It Works -" />
                 {/* <Section type="featured-games" title="Featured Games" /> */}
-                <Section type="testimonials" title="What Our Users Say" />
-                <Section type="news" title="Latest News and Articles" />
+                {/* <Section type="testimonials" title="What Our Users Say" /> */}
+                {/* <Section type="news" title="Latest News and Articles" /> */}
                 <Section type="faqs" />
                 <Section type="contact" />
             </main>
